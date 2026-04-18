59b5ee3c29c6e55dcdb7a4f580de9cfccea7cade
ming <hongming_1212@hotmail.com>
2026-04-18 17:30:56 +0800

    style: updated the app icon and logo used

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,10 @@ function App() {
         <div className="headerInner">
           <div className="brand">
             <div className="brandMark" aria-hidden="true">
-              <span className="brandDot" />
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="var(--accent)" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
+                <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
+              </svg>
             </div>
             <div className="brandText">
               <div className="brandTitle">React + JavaScript Interview FAQ</div>
